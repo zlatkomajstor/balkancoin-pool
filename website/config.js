99c65ae409858d4e69c97745d91129d0c98a10ee
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2018-05-18 20:13:23 +0200

    New pool added

--- website/config.js
+++ website/config.js
@@ -22,6 +22,7 @@ var networkStat = {
     "bkc": [
     	["pool.balkancoin.org", "http://miner.balkancoin.org:8147"],
     	["bkc.mininggood.com", "http://us2.mininggood.com:8147"],
-      ["youpool.io/BKC", "http://118.178.122.224:8118"]
+      ["youpool.io/BKC", "http://118.178.122.224:8118"],
+      ["npool.pw/bkc", "http://bkc.npool.pw:10040"]
     ]
 };
